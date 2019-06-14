01db70c969c4012d2d19a7d04a64b34cfe958c03
Cris Daniluk <cdaniluk@rhythmictech.com>
2019-06-14 15:02:37 -0400

    some fixes (still busted)

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 0.12"
 }
 
-data "aws_secretsmanager_secret" "private_key_secret" {
-  arn = var.private_key_secret 
-}
-
 module "acm_cert_tags" {
   source = "rhythmictech/asg-tag-transform/aws"
   version = "~> 1.0.0"
@@ -13,18 +9,15 @@ module "acm_cert_tags" {
 }
 
 resource "null_resource" "acm_external_cert" {
-  depends_on = [
-    "data.aws_secretsmanager_secret",
-  ]
 
   provisioner "local-exec" {
     interpreter = ["bash"]
     
     command = <<EOF
 aws --output json acm import-certificate \
-  --private-key "$(aws --output json secretsmanager get-secret-value --secret-id ${secret_id} | jq -r '.SecretString')" \
-  --certificate "${certificate_body}" \
-  --certificate_chain "${certificate_chain}"
+  --private-key "$(aws --output json secretsmanager get-secret-value --secret-id ${var.private_key_secret} | jq -r '.SecretString')" \
+  --certificate "${var.certificate_body}" \
+  --certificate_chain "${var.certificate_cabundle_body}"
 EOF
   }
 }
